bcd2a8ce881289a00e65b6e2e99661be8bbac0f0
Maia Kikvadze <mako.kikvadze@gmail.com>
2022-12-16 15:55:53 +0400

    selenide homework

--- src/test/java/SelenideAssertTests.java
+++ src/test/java/SelenideAssertTests.java
@@ -1,43 +1,41 @@
 import com.codeborne.selenide.*;
-import com.codeborne.selenide.selector.ByText;
-import com.codeborne.selenide.testng.SoftAsserts;
 import org.openqa.selenium.By;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import java.util.List;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-@Listeners({SoftAsserts.class})
+
 public class SelenideAssertTests {
 
     public SelenideAssertTests() {
-
-        Configuration.browserSize = ("2048x1536");
+               Configuration.browserSize = ("2048x1536");
         timeout = 10000;
         holdBrowserOpen = false;
         screenshots = true;
         reopenBrowserOnFail = true;
         assertionMode = AssertionMode.SOFT;
         reportsFolder = "src/main/resources/Reports";
+        savePageSource = false;
 
 
     }
 
     @Test
-    public void testOne()  {
+    public void testOne() {
         open("https://demoqa.com/books ");
-        //ჩვეულებრივი იქსპასით აკეთებს და მეორეში რა მერევა, ვერ გავიგე
-        // List<SelenideElement> books= $$(By.xpath("//div[contains(text(), 'Reilly Media')]//ancestor-or-self::div[@role='row']//div[2]//div//span//a[contains(text(), 'JavaScript')]"));
-        List<SelenideElement> books = $(withText("Reilly")).closest("[role=row]").find(("div"), 1)
-                .find("div").find("span").findAll(withText("JavaScript"));
+        ElementsCollection books = $(".rt-tbody").findAll(".rt-tr-group").filterBy(text("O'Reilly Media")).filterBy(text("Javascript"));
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(books.size(), 10);
-        softAssert.assertEquals(books.get(0).getText(), "Learning JavaScript Design Patterns");
-        books.stream().forEach(el -> el.click());
+        String firstMatch = books.first().find("div").find("div", 1).getText();
+        softAssert.assertEquals(firstMatch, "Learning JavaScript Design Patterns");
+        SelenideElement back =$(byId("addNewRecordButton"));
+        books.stream().forEach(b -> b.find("a").scrollTo().pressEnter());
+        //ვერ გადავდივარ უკან ვერანაირი ცდით
+        back();
+
         softAssert.assertAll();
 
     }
@@ -45,11 +43,9 @@ public class SelenideAssertTests {
     @Test
     public void testTwo() {
         open("https://demoqa.com/books ");
-        //find-ის ნაცვლად დოლარის ნიშნებიც ჩავსვი და ისევ ამაოდ
-        List<SelenideElement> books = $$(By.xpath("//div[contains(text(), 'Reilly Media')]//ancestor-or-self::div[@role='row']//div[2]//div//span//a[contains(text(), 'JavaScript')]"));
-        // $$("img[src*='book']").stream.forEach(img -> img.shouldBe(not(empty));
-        List <SelenideElement> images = $$(By.xpath("//img[contains(@src, 'book')]"));
-        images.stream().forEach(img -> img.shouldBe(not(empty)));
+        ElementsCollection books = $(".rt-tbody").$$(".rt-tr-group").filterBy(text("O'Reilly Media")).filterBy(text("Javascript"));
+        ElementsCollection images = $$(By.xpath("//img[contains(@src, 'book')]"));
+        images.stream().forEach(img -> img.isImage());
 
     }
 
